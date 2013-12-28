93500b2b53477d3991b934326a4e36e5d332b278
Gerardo Cadau <gerardocadau@gmail.com>
2013-12-28 19:56:05 -0300

    Adding classes to user menu

--- sites/all/modules/magdalena/templates/block--user-menu.tpl.php
+++ sites/all/modules/magdalena/templates/block--user-menu.tpl.php
@@ -1,6 +1,6 @@
 <ul class="user-menu-top">
-	<li><?php print t('Bienvenido, !user', array('!user' => $user->name)); ?></li>
-	<li><?php print l(t('Mi perfil'), 'user') ?></li>
-	<li><?php print l(t('Mi historial'), 'user') ?></li>
-	<li><?php print l(t('Cerrar sesión'), 'user/logout') ?></li>
+	<li class="user-welcome"><?php print t('Bienvenido, !user', array('!user' => $user->name)); ?></li>
+	<li class="user-profile"><?php print l(t('Mi perfil'), 'user') ?></li>
+	<li class="user-history"><?php print l(t('Mi historial'), 'user') ?></li>
+	<li class="user-logout"><?php print l(t('Cerrar sesión'), 'user/logout') ?></li>
 </ul>
