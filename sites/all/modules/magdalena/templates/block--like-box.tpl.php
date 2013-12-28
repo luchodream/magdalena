ce6417088f5ccbf1745137df9f18879d74816b91
Gerardo Cadau <gerardocadau@gmail.com>
2013-12-28 18:36:57 -0300

    Adding the like box

---
+++ sites/all/modules/magdalena/templates/block--like-box.tpl.php
@@ -0,0 +1,9 @@
+<h2><?php print t('SÚMATE A NUESTRA COMUNIDAD') ?></h2>
+<h3><?php print t('y obtén beneficios exclusivos') ?></h3>
+<div class="fb-like-box" data-href="<?php print variable_get('fb_like_page', 'https://www.facebook.com/pages/Granja-Magdalena/197746717067278'); ?>" 
+	data-colorscheme="light" data-show-faces="true" 
+	data-header="false" 
+	data-width="225"
+	data-stream="false" 
+	data-show-border="true">
+</div>
