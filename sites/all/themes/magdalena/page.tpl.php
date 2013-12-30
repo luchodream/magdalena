cb243b5410062d752021f626159b95f987fe7007
Gerardo Cadau <gerardocadau@gmail.com>
2013-12-30 02:17:51 -0300

    Hide page title in some pages

--- sites/all/themes/magdalena/page.tpl.php
+++ sites/all/themes/magdalena/page.tpl.php
@@ -39,7 +39,7 @@
           <?php if ($tabs): ?><div id="tabs-wrapper" class="clearfix"><?php endif; ?>
           <?php print render($title_prefix); ?>
           <?php if ($title): ?>
-            <h1<?php print $tabs ? ' class="with-tabs"' : '' ?>><?php print $title ?></h1>
+            <h1<?php print $tabs ? ' class="page-title with-tabs"' : ' class="page-title"' ?>><?php print $title ?></h1>
           <?php endif; ?>
           <?php print render($title_suffix); ?>
           <?php if ($tabs): ?><?php print render($tabs); ?></div><?php endif; ?>
