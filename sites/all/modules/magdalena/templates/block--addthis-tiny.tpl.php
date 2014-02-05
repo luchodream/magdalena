9b15f3f0f58cd6fde91d2656a8a3298bcff1336f
Gerardo Cadau <gerardocadau@gmail.com>
2014-02-05 01:06:26 -0300

    Adding back button and share buttons on product detail

---
+++ sites/all/modules/magdalena/templates/block--addthis-tiny.tpl.php
@@ -0,0 +1,14 @@
+<div class="addthis-product-wrapper">
+	<div class="addthis-product-share addthis_toolbox addthis_default_style addthis_32x32_style">
+		<a class="addthis_button_facebook"></a>
+		<a class="addthis_button_twitter"></a>
+		<a class="addthis_button_pinterest_share"></a>
+	</div>
+	<div class="addthis-product-like addthis_toolbox addthis_default_style">
+		<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
+	</div>
+</div>
+<div class="quality-wrapper">
+	<div class="quality-icon"></div>
+	<div class="quality-text"><?php print t('Al menos !days desde la fecha de despacho.', array('!days' => '<span class="quality-days">' . t('180 días') . '</span>')); ?></div>
+</div>
---
+++ sites/all/modules/magdalena/templates/block--back-button.tpl.php
@@ -0,0 +1,3 @@
+<div class="back-button-wrapper">
+	<button onClick="javascript:window.history.back()" class="back-button"><?php print t('Volver'); ?></button>
+</div>
