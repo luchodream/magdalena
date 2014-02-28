be8b37845aa304f0cec89522db8f799af7006f9d
Gerardo Cadau <gerardocadau@gmail.com>
2014-02-28 16:33:21 -0300

    Removing unused js from magdalena module

--- sites/all/modules/magdalena/js/magdalena.js
+++ sites/all/modules/magdalena/js/magdalena.js
@@ -11,18 +11,3 @@ window.fbAsyncInit = function() {
     });
     // Additional initialization code such as adding Event Listeners goes here 
 };
-
-(function ($) {
-  // Add a spinner on quantity widget.
-  Drupal.behaviors.quantitySpinner = {
-    attach: function ( context, settings ) {
-      $('.form-item-quantity input, input[name*="edit_quantity"]', context).once('spinner', function(){
-      	$(this).spinner({
-        	min: 1,
-        	max: 9999,
-        	increment: 'fast'
-      	});
-      })
-    }
-  }
-})(jQuery);
